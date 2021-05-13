b346a69588663d3a92c94efb19cfde9234841c7f
Desiderio Pineda <desi@politicaldata.com>
2021-05-12 23:06:51 -0700

    Dashboard header

---
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,18 @@
+import 'package:admin/constants.dart';
+import 'package:flutter/material.dart';
+
+import 'components/header.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [Header()],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,5 +1,6 @@
+import 'package:admin/components/side_menu.dart';
+import 'package:admin/screens/dashboard/dashboard_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class MainScreen extends StatelessWidget {
   @override
@@ -7,94 +8,18 @@ class MainScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(
                 // default flex = 1
                 // and takes 1/6 part of the screen
-                child: Column(
-              children: [
-                DrawerHeader(
-                  child: Image.asset("assets/images/logo.png"),
-                ),
-                DrawerListTile(
-                  title: "Dashboard",
-                  svgSrc: "assets/icons/menu_dashbord.svg",
-                  press: () => {},
-                ),
-                DrawerListTile(
-                  title: "Transaction",
-                  svgSrc: "assets/icons/menu_tran.svg",
-                  press: () => {},
-                ),
-                DrawerListTile(
-                  title: "Task",
-                  svgSrc: "assets/icons/menu_task.svg",
-                  press: () => {},
-                ),
-                DrawerListTile(
-                  title: "Documents",
-                  svgSrc: "assets/icons/menu_doc.svg",
-                  press: () => {},
-                ),
-                DrawerListTile(
-                  title: "Store",
-                  svgSrc: "assets/icons/menu_store.svg",
-                  press: () => {},
-                ),
-                DrawerListTile(
-                  title: "Notifications",
-                  svgSrc: "assets/icons/menu_notification.svg",
-                  press: () => {},
-                ),
-                DrawerListTile(
-                  title: "Profile",
-                  svgSrc: "assets/icons/menu_profile.svg",
-                  press: () => {},
-                ),
-                DrawerListTile(
-                  title: "Settings",
-                  svgSrc: "assets/icons/menu_setting.svg",
-                  press: () => {},
-                ),
-              ],
-            )),
+                child: SideMenu()),
             Expanded(
                 // It takes 5/6 part of the screen
                 flex: 5,
-                child: Container(
-                  color: Colors.blue,
-                ))
+                child: DashboardScreen())
           ],
         ),
-      ),
-    );
-  }
-}
-
-class DrawerListTile extends StatelessWidget {
-  const DrawerListTile({
-    Key key,
-    @required this.title,
-    @required this.svgSrc,
-    @required this.press,
-  }) : super(key: key);
-
-  final String title, svgSrc;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: press,
-      horizontalTitleGap: 0.0,
-      leading: SvgPicture.asset(
-        svgSrc,
-        color: Colors.white54,
-        height: 16,
-      ),
-      title: Text(
-        title,
-        style: TextStyle(color: Colors.white54),
       ),
     );
   }
